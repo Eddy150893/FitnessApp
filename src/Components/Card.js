6df6d37db7ab633d291db02063cae0007277d552
Eddy150893 <eddypaz150893@gmail.com>
2019-09-02 17:38:10 -0600

    init

---
+++ src/Components/Card.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import add from '../images/add.png'
+import exerciseImg from '../images/exercise.png'
+import circlesImg from '../images/circles.png'
+import './styles/Card.css'
+
+
+class Card extends React.Component{
+	constructor(props){
+		super(props)
+		this.state={
+			image:add
+		}
+	}
+
+	componentDidMount(){
+		setTimeout(()=>{
+			this.setState({
+				image:exerciseImg
+			})
+		},5000)
+	}
+	render(){
+		const{title,description,img,leftColor,rightColor}=this.props/*esto es destructuring y sirve para igualar a un array asociativo las propiedades*/
+		return (
+			<div className="card mx-auto Fitness-Card"
+			style={{
+				backgroundImage:`url(${circlesImg}),linear-gradient(to right, ${leftColor}, ${rightColor})`
+			}}>
+				<div className="card-body">
+					<div className="row center">
+						<div className="col-6">
+							<img src={img} className="float-right"/>
+						</div>
+						<div className="col-6 Fitness-Card-Info">
+							<h1>{title}</h1>
+							<p>{description}</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			)
+	}
+}
+
+export default Card
