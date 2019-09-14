2a27be2aa11268b8edf20dcad1b35b87097f9628
eddypaz <eddypaz150893@gmail.com>
2019-09-14 09:36:50 -0600

    despues de migracion 1

--- src/pages/ExerciseNew.js
+++ src/pages/ExerciseNew.js
@@ -1,78 +1,20 @@
 import React from 'react'
 import ExerciseForm from '../Components/ExerciseForm'
 import Card from '../Components/Card'
-import FatalError from './500'
-class ExerciseNew extends React.Component {
-	state={
-		form: {
-			title: '',
-			description: '',
-			img: '',
-			leftColor: '',
-			rightColor: ''
-		},//El estate se inicializa con un objeto pero dicho objeto esta vacio
-        loading:false,
-        error:null
-	}
-	handleChange = e => {
-        this.setState({
-        	form:{
-        	...this.state.form,//mantiene la informacion de los campos
-            [e.target.name]: e.target.value//le asigna al atributo entre parentesis el valor de dicho atributo en el formulario
-        	}
-        	
-        })
-    }
-
-     handleSubmit = async e => {
-        this.setState({
-            loading:true
-        })
-        e.preventDefault()//esto hace que no recarge la pagina
-        /*console.log(this.state)*/
-        try{
-        	let config = {
-        		method: 'POST',
-        		headers:{
-        			'accept':'application/json',
-        			'Content-Type':'application/json'
-        		},
-        		body: JSON.stringify(this.state.form)
-        	}
-        	let res = await fetch('http://localhost:8000/api/exercises',config)
-            let json = await res.json()
-        	this.setState({
-                loading:false
-            })
-            this.props.history.push('/exercise')
-        }catch(error){
-            this.setState({
-                loading:false,
-                error
-            })
-        }
-    }
-
-    render(){
-        if(this.state.error)
-            return <FatalError/>
-    	return(
-    		<div className="row">
+const ExerciseNew = ({form,onChange,onSubmit}) =>(
+	<div className="row">
     			<div className="col-sm">
-    			<Card {...this.state.form }/>
+    			<Card {...form }/>
     			</div>
     			<div className="col-sm">
     				<ExerciseForm
-    				onChange={this.handleChange}
-    				onSubmit={this.handleSubmit}
-    				form={this.state.form}
+    				onChange={onChange}
+    				onSubmit={onSubmit}
+    				form={form}
     				/>
     			</div>
     		</div>
-    		
-    	)
-    }
+)
 
-}
 
 export default ExerciseNew
---
+++ src/pages/ExerciseNewContainer.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import Loading from '../Components/Loading'
+import FatalError from './500'
+import ExerciseNew from './ExerciseNew'
+
+class ExerciseNewContainer extends React.Component {
+	state={
+		form: {
+			title: '',
+			description: '',
+			img: '',
+			leftColor: '',
+			rightColor: ''
+		},//El estate se inicializa con un objeto pero dicho objeto esta vacio
+        loading:false,
+        error:null
+	}
+	handleChange = e => {
+        this.setState({
+        	form:{
+        	...this.state.form,//mantiene la informacion de los campos
+            [e.target.name]: e.target.value//le asigna al atributo entre parentesis el valor de dicho atributo en el formulario
+        	}
+        	
+        })
+    }
+
+     handleSubmit = async e => {
+        this.setState({
+            loading:true
+        })
+        e.preventDefault()//esto hace que no recarge la pagina
+        /*console.log(this.state)*/
+        try{
+        	let config = {
+        		method: 'POST',
+        		headers:{
+        			'accept':'application/json',
+        			'Content-Type':'application/json'
+        		},
+        		body: JSON.stringify(this.state.form)
+        	}
+        	let res = await fetch('http://localhost:8000/api/exercises',config)
+            let json = await res.json()
+        	this.setState({
+                loading:false
+            })
+            this.props.history.push('/exercise')
+        }catch(error){
+            this.setState({
+                loading:false,
+                error
+            })
+        }
+    }
+
+    render(){
+        if(this.state.loading)
+            return <Loading />
+        if(this.state.error)
+            return <FatalError/>
+    	return <ExerciseNew 
+            form={this.state.form}
+            onChange={this.handleChange}
+            onSubmit={this.handleSubmit}
+        />
+    }
+
+}
+
+export default ExerciseNewContainer
